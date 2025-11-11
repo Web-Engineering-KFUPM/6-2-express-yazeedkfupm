fcaa2d1230e98fc88b9d07820b42a1924a412a15
yazeedkfupm <s202164390@kfupm.edu.sa>
2025-11-11 23:19:53 +0300

    lab 5-8

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -1,11 +1,22 @@
 import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
-
-{/*write code for cors*/}
-
-
-{/*write code to define routes*/}
+const app = express();
+app.use(cors());
 
 
-{/*write code to create server*/}
+app.get("/", (req, res) => {
+  res.send("Welcome to the Quote Generator API");
+});
+
+
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
